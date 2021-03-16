4bd34815dd79b028a5cf9ad41a9260efd7d59bb2
SantosMaycon <mayconalves.santos14@gmail.com>
2021-03-16 13:39:48 -0300

    add player

--- src/scenes/SceneGame.js
+++ src/scenes/SceneGame.js
@@ -8,6 +8,7 @@ class SceneGame extends Phaser.Scene {
   constructor() {
     super();
     this.platforms;
+    this.player;
   }
 
   preload() {
@@ -30,6 +31,31 @@ class SceneGame extends Phaser.Scene {
     this.platforms.create(600, 400, "ground");
     this.platforms.create(50, 250, "ground");
     this.platforms.create(750, 220, "ground");
+
+    this.player = this.physics.add.sprite(100, 450, "dude");
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
   }
 }
 
